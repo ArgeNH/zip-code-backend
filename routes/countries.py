1c6d3bf4e79e298aeee2756b1597903dab43f70e
ArgeNH <arge.nino@uptc.edu.co>
2023-05-07 01:40:02 -0500

    refactor: move patterns

--- routes/countries.py
+++ routes/countries.py
@@ -100,3 +100,96 @@ countries_dict = {
     (4, 3, 1): ["Malta", "Montserrat"],
     (3, 1, 0): ["Esuatini", "South Africa"]
 }
+
+patterns = {
+    # Codigo postal de ESTADOS UNIDOS
+    "ZIPCODE_US": [{"SHAPE": "dddd"}, {"ORTH": "-"}, {"SHAPE": "ddddd"}],
+    # Codigo postal de CANADA
+    "ZIPCODE_CA": [
+        {"SHAPE": "XdX"},
+        {"IS_SPACE": True, "OP": "?"},
+        {"SHAPE": "dXd"},
+    ],
+    # Codigo postal de COLOMBIA, Belarus,
+    "ZIPCODE_CO": [{"SHAPE": "dddddd"}],
+    # Codigo postal de AUSTRALIA, AFGANISTAN,
+    # Albania, Armenia, Argentina, Austria,
+    #  Bangladesh, Belgica, Bulgaria, Cabo Verde
+    # Codigo postal de ESPAÑA, Aland Islands, Algeria, Bhutan, Cambodia
+    "ZIPCODE_ES": [{"SHAPE": "ddddd"}],
+    "ZIPCODE_GR": [
+        {"SHAPE": "ddd"},
+        {"IS_SPACE": True, "OP": "?"},
+        {"SHAPE": "dd"}
+    ],  # Codigo postal de GRECIA
+    "ZIPCODE_UK_ONE": [
+        {"SHAPE": "XXd"},
+        {"IS_SPACE": True, "OP": "?"},
+        {"SHAPE": "dXX"}
+    ],
+    "ZIPCODE_UK_TWO": [
+        {"SHAPE": "XXdd"},
+        {"IS_SPACE": True, "OP": "?"},
+        {"SHAPE": "dXX"}
+    ],
+    "ZIPCODE_IRLANDA": [{"SHAPE": "Xdd"}],  # Codigo postal de IRLANDA
+    "ZIPCODE_AD": [{"SHAPE": "XXddd"}],  # Codigo postal de ANDORRA
+    # Codigo postal de Azerbaijan, Islas Virgenes Britanicas, Brunei
+    "ZIPCODE_AZ": [{"SHAPE": "XXdddd"}],
+    # Codigo postal de ANGUILA
+    "ZIPCODE_AI": [{"SHAPE": "XX-dddd"}],
+    # Codigo postal de Argentina
+    "ZIPCODE_AR_ONE": [{"SHAPE": "XddddXXX"}],
+    "ZIPCODE_AR_TWO": [{"SHAPE": "Xdddd"}],  # Codigo postal de Argentina
+    "ZIPCODE_BB": [{"SHAPE": "XXddddd"}],  # Codigo postal de Barbados
+    "ZIPCODE_BM_ONE": [
+        {"SHAPE": "XX"},
+        {"IS_SPACE": True, "OP": "?"},
+        {"SHAPE": "dd"}
+    ],  # Codigo postal de Bermuda
+    "ZIPCODE_BM_TWO": [
+        {"SHAPE": "XX"},
+        {"IS_SPACE": True, "OP": "?"},
+        {"SHAPE": "XX"}
+    ],  # Codigo postal de Bermuda
+    "ZIPCODE_BR": [{"SHAPE": "ddddd-ddd"}],  # Brazil
+    "ZIPCODE_IO": [
+        {"SHAPE": "XXdX"},
+        {"IS_SPACE": True, "OP": "?"},
+        {"SHAPE": "XX"}
+    ],  # British Indian Ocean Territory
+    "ZIPCODE_KY": [{"SHAPE": "XXd-dddd"}],  # Islas Caiman
+    "ZIPCODE_AU": [{"SHAPE": "dddd"}],
+        "ZIPCODE_CL": [{"SHAPE": "ddddddd"}],  # Chile
+        "ZIPCODE_FK": [
+            {"SHAPE": "XXXX"},
+            {"IS_SPACE": True, "OP": "?"},
+            {"SHAPE": "dXX"}
+    ],  # Islas Malvinas
+    "ZIPCODE_FO": [{"SHAPE": "ddd"}],  # Islas Feroe
+    "ZIPCODE_GF": [{"SHAPE": "973dd"}],  # Guyana Francesa
+        "ZIPCODE_GFP": [{"SHAPE": "987dd"}],  # Polinesia Frances
+        "ZIPCODE_NC": [{"SHAPE": "988dd"}],  # Nueva Caledonia
+        "ZIPCODE_GP": [{"SHAPE": "971dd"}],  # Guadalope
+        "ZIPCODE_DE": [{"SHAPE": "dd"}],  # Alemania 1
+        "ZIPCODE_JM": [{"SHAPE": "XXXXdd"}],  # Jamaica
+        "ZIPCODE_JP": [{"SHAPE": "ddd-dddd"}],  # Japon
+        "ZIPCODE_MT": [
+            {"SHAPE": "XXX"},
+            {"IS_SPACE": True, "OP": "?"},
+            {"SHAPE": "dddd"}
+    ],  # Malta
+    "ZIPCODE_NL": [
+            {"SHAPE": "dddd"},
+            {"IS_SPACE": True, "OP": "?"},
+            {"SHAPE": "XX"}
+    ],  # Holanda
+    "ZIPCODE_PT": [
+            {"SHAPE": "dddd"},
+            {"IS_SPACE": True, "OP": "?"},
+            {"SHAPE": "ddd"}
+    ],  # Holanda
+    # Saint Vincent and the Grenadines
+    "ZIPCODE_VC": [{"SHAPE": "XXdddd"}],
+        "ZIPCODE_SZ": [{"SHAPE": "Xddd"}],
+}
--- routes/retrieveData.py
+++ routes/retrieveData.py
@@ -1,1 +1,3 @@
 from pandas import read_excel
+
+df = read_excel("/Users/argenino/Downloads/geonames-postal-code@public.xlsx")
